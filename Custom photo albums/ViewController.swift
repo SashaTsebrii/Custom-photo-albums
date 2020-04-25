42d98fa0223164d1bf78e44742c0c36209728a5a
SashaTsebrii <sashatsebrii@gmail.com>
2020-04-25 16:58:41 +0300

    Initial Commit

---
+++ Custom photo albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom photo albums
+//
+//  Created by Aleksandr Tsebrii on 4/25/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
