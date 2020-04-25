35bdcb879fe56246a23f31ea792d609d4ec61eb8
SashaTsebrii <sashatsebrii@gmail.com>
2020-04-25 17:19:18 +0300

    Initial commit.

---
+++ Custom photo albums/PhotoAlbumsController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoAlbumsController.swift
+//  Custom photo albums
+//
+//  Created by Aleksandr Tsebrii on 4/25/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
