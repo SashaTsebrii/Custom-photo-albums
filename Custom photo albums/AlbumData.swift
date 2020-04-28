2570bbfaf1eb20e06fe0aedf70faaceaafa3304f
SashaTsebrii <sashatsebrii@gmail.com>
2020-04-28 21:45:06 +0300

    Implement model objects.

---
+++ Custom photo albums/AlbumData.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumData.swift
+//  Custom photo albums
+//
+//  Created by Aleksandr Tsebrii on 4/28/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct AlbumData {
+    var title: String?
+    var items: Int?
+    var photos: [PhotoData]?
+}
---
+++ Custom photo albums/PhotoData.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoData.swift
+//  Custom photo albums
+//
+//  Created by Aleksandr Tsebrii on 4/28/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct PhotoData {
+    var image: UIImage?
+}
