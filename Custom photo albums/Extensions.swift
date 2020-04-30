4cd4e42d934948144593df6e6e08a74bdc09cdde
SashaTsebrii <sashatsebrii@gmail.com>
2020-04-30 05:56:27 +0300

    Implement ability to display fetched photo from albums.

--- Custom photo albums/Extensions.swift
+++ Custom photo albums/Extensions.swift
@@ -35,3 +35,10 @@ extension UIColor {
     }
     
 }
+
+extension UICollectionView {
+    func indexPathsForElements(in rect: CGRect) -> [IndexPath] {
+        let allLayoutAttributes = collectionViewLayout.layoutAttributesForElements(in: rect)!
+        return allLayoutAttributes.map { $0.indexPath }
+    }
+}
--- Custom photo albums/PhotosController.swift
+++ Custom photo albums/PhotosController.swift
@@ -7,21 +7,27 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 class PhotosController: UIViewController {
-    
+
     // MARK: Variables
+
+    var fetchResult: PHFetchResult<PHAsset>!
+    var assetCollection: PHAssetCollection!
+    var availableWidth: CGFloat = 0
     
-    var album: AlbumData? {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
-    
+    var collectionViewFlowLayout = UICollectionViewFlowLayout()
+
+    fileprivate let imageManager = PHCachingImageManager()
+    fileprivate var thumbnailSize: CGSize!
+    fileprivate var previousPreheatRect = CGRect.zero
+
     // MARK: Properties
-    
+
     lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
@@ -30,12 +36,12 @@ class PhotosController: UIViewController {
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
-    
+
     override func loadView() {
         super.loadView()
-        
+
         view.backgroundColor = UIColor.Design.background
-        
+
         view.addSubview(collectionView)
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -43,47 +49,197 @@ class PhotosController: UIViewController {
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Create Import button
+        let importBarButton = UIBarButtonItem(title: "Import", style: .plain, target: self, action: #selector(importBarButtonTapped(_:)))
+        self.navigationItem.rightBarButtonItem = importBarButton
+
+        resetCachedAssets()
+        PHPhotoLibrary.shared().register(self)
+
+        // Reaching this point without a segue means that this AssetGridViewController became visible at app launch. As such, match the behavior of the segue from the default "All Photos" view.
+        if fetchResult == nil {
+            let allPhotosOptions = PHFetchOptions()
+            allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+            fetchResult = PHAsset.fetchAssets(with: allPhotosOptions)
+        }
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let width = view.bounds.inset(by: view.safeAreaInsets).width
+        // Adjust the item size if the available width has changed.
+        if availableWidth != width {
+            availableWidth = width
+            let columnCount = (availableWidth / 80).rounded(.towardZero)
+            let itemLength = (availableWidth - columnCount - 1) / columnCount
+            collectionViewFlowLayout.itemSize = CGSize(width: itemLength, height: itemLength)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        navigationItem.title = "Photos"
+        // Determine the size of the thumbnails to request from the PHCachingImageManager.
+        let scale = UIScreen.main.scale
+        let cellSize = collectionViewFlowLayout.itemSize
+        thumbnailSize = CGSize(width: cellSize.width * scale, height: cellSize.height * scale)
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateCachedAssets()
+    }
+
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
+    }
+
+    // MARK: Actions
+
+    @objc func importBarButtonTapped(_ sender: UIBarButtonItem) {
+
+    }
+
+    // MARK: Asset Caching
+
+    fileprivate func resetCachedAssets() {
+        imageManager.stopCachingImagesForAllAssets()
+        previousPreheatRect = .zero
+    }
+    /// - Tag: UpdateAssets
+    fileprivate func updateCachedAssets() {
+        // Update only if the view is visible.
+        guard isViewLoaded && view.window != nil else { return }
+
+        // The window you prepare ahead of time is twice the height of the visible rect.
+        let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.bounds.size)
+        let preheatRect = visibleRect.insetBy(dx: 0, dy: -0.5 * visibleRect.height)
+
+        // Update only if the visible area is significantly different from the last preheated area.
+        let delta = abs(preheatRect.midY - previousPreheatRect.midY)
+        guard delta > view.bounds.height / 3 else { return }
+
+        // Compute the assets to start and stop caching.
+        let (addedRects, removedRects) = differencesBetweenRects(previousPreheatRect, preheatRect)
+        let addedAssets = addedRects
+            .flatMap { rect in collectionView.indexPathsForElements(in: rect) }
+            .map { indexPath in fetchResult.object(at: indexPath.item) }
+        let removedAssets = removedRects
+            .flatMap { rect in collectionView.indexPathsForElements(in: rect) }
+            .map { indexPath in fetchResult.object(at: indexPath.item) }
+
+        // Update the assets the PHCachingImageManager is caching.
+        imageManager.startCachingImages(for: addedAssets,
+                                        targetSize: thumbnailSize, contentMode: .aspectFill, options: nil)
+        imageManager.stopCachingImages(for: removedAssets,
+                                       targetSize: thumbnailSize, contentMode: .aspectFill, options: nil)
+        // Store the computed rectangle for future comparison.
+        previousPreheatRect = preheatRect
+    }
+    
+    fileprivate func differencesBetweenRects(_ old: CGRect, _ new: CGRect) -> (added: [CGRect], removed: [CGRect]) {
+        if old.intersects(new) {
+            var added = [CGRect]()
+            if new.maxY > old.maxY {
+                added += [CGRect(x: new.origin.x, y: old.maxY,
+                                 width: new.width, height: new.maxY - old.maxY)]
+            }
+            if old.minY > new.minY {
+                added += [CGRect(x: new.origin.x, y: new.minY,
+                                 width: new.width, height: old.minY - new.minY)]
+            }
+            var removed = [CGRect]()
+            if new.maxY < old.maxY {
+                removed += [CGRect(x: new.origin.x, y: new.maxY,
+                                   width: new.width, height: old.maxY - new.maxY)]
+            }
+            if old.minY < new.minY {
+                removed += [CGRect(x: new.origin.x, y: old.minY,
+                                   width: new.width, height: new.minY - old.minY)]
+            }
+            return (added, removed)
+        } else {
+            return ([new], [old])
+        }
+    }
+    
+    func updateStaticImage(fromImageView imageView: UIImageView, indexPath: IndexPath) {
+        // Prepare the options to pass when fetching the (photo, or video preview) image.
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .highQualityFormat
+        options.isNetworkAccessAllowed = true
+        options.progressHandler = { progress, _, _, _ in
+        }
+        
+        let scale = UIScreen.main.scale
+        let targetSize =  CGSize(width: imageView.bounds.width * scale, height: imageView.bounds.height * scale)
+        
+        let asset = fetchResult.object(at: indexPath.item)
+        
+        PHImageManager.default().requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options,
+                                              resultHandler: { image, _ in
+                                                // PhotoKit finished the request.
+                                                
+                                                // If the request succeeded, show the image view.
+                                                guard let image = image else { return }
+                                                
+                                                // Show the image.
+                                                // FIXME: Save image.
+        })
+    }
+
 }
 
 extension PhotosController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
+
     // MARK: UICollectionViewDataSource
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let photos = album?.photos {
-            return photos.count
-        } else {
-            return 0
-        }
+        return fetchResult.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as! PhotoCell
-        if let photos = album?.photos {
-            let photo = photos[indexPath.item]
-            cell.photo = photo
+        let asset = fetchResult.object(at: indexPath.item)
+        // Dequeue a GridViewCell.
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as? PhotoCell
+            else { fatalError("Unexpected cell in collection view") }
+        
+        // Add a badge to the cell if the PHAsset represents a Live Photo.
+        if asset.mediaSubtypes.contains(.photoLive) {
+            cell.livePhotoBadgeImage = PHLivePhotoView.livePhotoBadgeImage(options: .overContent)
         }
+        
+        // Request an image for the asset from the PHCachingImageManager.
+        cell.representedAssetIdentifier = asset.localIdentifier
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            // UIKit may have recycled this cell by the handler's activation time. Set the cell's thumbnail image only if it's still showing the same asset.
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                cell.thumbnailImage = image
+            }
+        })
         return cell
     }
-    
+
     // MARK: UICollectionViewDelegate
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
         // FIXME: Implement select cell.
+        guard let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell else { return }
+        updateStaticImage(fromImageView: cell.photoImageView, indexPath: indexPath)
+
     }
-    
+
     // MARK: UICollectionViewDelegateFlowLayout
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.bounds.width / 3, height: collectionView.bounds.width / 3)
     }
@@ -100,5 +256,49 @@ extension PhotosController: UICollectionViewDataSource, UICollectionViewDelegate
         return 0
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateCachedAssets()
+    }
+
 }
 
+extension PhotosController: PHPhotoLibraryChangeObserver {
+
+    // MARK: PHPhotoLibraryChangeObserver
+
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else { return }
+
+        // Change notifications may originate from a background queue. As such, re-dispatch execution to the main queue before acting on the change, so you can update the UI.
+        DispatchQueue.main.sync {
+            // Hang on to the new fetch result.
+            fetchResult = changes.fetchResultAfterChanges
+            // If we have incremental changes, animate them in the collection view.
+            if changes.hasIncrementalChanges {
+                // Handle removals, insertions, and moves in a batch update.
+                collectionView.performBatchUpdates({
+                    if let removed = changes.removedIndexes, !removed.isEmpty {
+                        collectionView.deleteItems(at: removed.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    if let inserted = changes.insertedIndexes, !inserted.isEmpty {
+                        collectionView.insertItems(at: inserted.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    changes.enumerateMoves { fromIndex, toIndex in
+                        self.collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+                                                to: IndexPath(item: toIndex, section: 0))
+                    }
+                })
+                // We are reloading items after the batch update since `PHFetchResultChangeDetails.changedIndexes` refers to items in the *after* state and not the *before* state as expected by `performBatchUpdates(_:completion:)`.
+                if let changed = changes.changedIndexes, !changed.isEmpty {
+                    collectionView.reloadItems(at: changed.map({ IndexPath(item: $0, section: 0) }))
+                }
+            } else {
+                // Reload the collection view if incremental changes are not available.
+                collectionView.reloadData()
+            }
+            resetCachedAssets()
+        }
+    }
+}
